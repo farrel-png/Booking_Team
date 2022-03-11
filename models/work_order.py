9dfc66446a2b6db9c9bde4c947b089f943013f4d
farrel-png <farrel160599@gmail.com>
2022-03-11 08:34:37 +0700

    WorkOrder

--- models/work_order.py
+++ models/work_order.py
@@ -5,4 +5,20 @@ class WorkOrder(models.Model):
     _name = 'work.order'
     _description = 'Work Order'
 
-    name = fields.Char(string='Name')
+    booking_order_reference = fields.Many2one('sale.order', string='Booking Order Reference')
+    team = fields.Many2one(comodel_name='service_team', string='Team', required=True)
+    team_members = fields.Many2many(comodel_name='res.users', string='Team Member')
+    planned_start = fields.Datetime('Planned Start', required=True, default=lambda self: fields.Date.Today())
+    planned_end = fields.Datetime('Planned End', required=True)
+    date_start = fields.Datetime('Date Start', readonly=True)
+    date_end = fields.Datetime('Date End', readonly=True)
+    state = fields.Selection(string='State', selection=[
+        ('pending', 'Pending'), 
+        ('in progress', 'In Progress'),
+        ('done', 'Done'),
+        ('cancelled', 'Cancelled'),
+        ])
+    notes = fields.Text(string='Notes')
+    
+    
+    
