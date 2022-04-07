5697420ae52665eac21d39f8e9bfe4b08cf50fa6
farrel-png <farrel160599@gmail.com>
2022-04-07 13:12:39 +0700

    Second Big Commit

--- __manifest__.py
+++ __manifest__.py
@@ -16,8 +16,9 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Sales',
     'version': '0.1',
+    'application': True,
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -29,6 +30,8 @@
         'views/templates.xml',
         'views/menu.xml',
         'views/service_team_views.xml',
+        'view/work_order_views.xml',
+        'view/sale_order_views.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
--- models/work_order.py
+++ models/work_order.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
 class WorkOrder(models.Model):
@@ -6,9 +6,9 @@ class WorkOrder(models.Model):
     _description = 'Work Order'
 
     wo_number = fields.Char(string='WO Number', readonly=True, copy=False)
-    
-    booking_order_reference = fields.Many2one('sale.order', string='Booking Order Reference')
+    bo_reference = fields.Many2one(comodel_name='sale.order', required=True)
     team = fields.Many2one(comodel_name='service_team', string='Team', required=True)
+    team_leader = fields.Many2one(comodel_name='res.users', string='Team Leader', required=True)
     team_members = fields.Many2many(comodel_name='res.users', string='Team Member')
     planned_start = fields.Datetime('Planned Start', required=True, default=lambda self: fields.Date.Today())
     planned_end = fields.Datetime('Planned End', required=True)
@@ -21,6 +21,35 @@ class WorkOrder(models.Model):
         ('cancelled', 'Cancelled'),
         ])
     notes = fields.Text(string='Notes')
+    
+    @api.model
+    def create(self,values):
+        if values.get('wo_number', _('New')) == _('New'):
+            if 'company_id' in values:
+                values['wo_number'] = self.env['ir.sequence'].with_context(
+                    force_company=values['company_id']
+                ).next_by_code('work.order') or _('New')
+            else:
+                values['wo_number'] = self.env['ir.sequence'].next_by_code(
+                    'work.order'
+                ) or _('New')
+        
+        return super(WorkOrder, self).create(values)
+
+    def start_work(self):
+        return self.write({'state': 'in_progress', 'date_start': field.Datetime.now()})
+
+    def end_work(self):
+        return self.write({'state': 'done', 'date_end': field.Datetime.now()})
+        
+    def reset(self):
+        return self.write({'state': 'pending', 'date_start': ''})
+
+    def cancel(self):
+        return self.write({'state': 'cancel'})
+        
+                
+                
     
     
     
